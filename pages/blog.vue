5a6f41b6d04e3b8624d02b3d8f7a277c0099afb5
kamamesi <yugami333@gmail.com>
2022-12-03 18:17:22 +0900

    create pages for vue practice

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>ブログページ</h1>
+        <p class="text">こんにちは</p>
+    </div>
+
+</template>
+
+<script setup></script>
+
+<style>
+h1 {
+    color: red
+}
+.text {
+    color: green;
+    letter-spacing: 20px;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Contactページ</h1>
+</template>
